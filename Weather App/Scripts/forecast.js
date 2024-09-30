0d9fa049e05a73b23f246f4a4e11a264d63fe48a
tanmay-37 <talekartk@rknec.edu>
2024-09-30 11:31:38 +0530

    Weather app

---
+++ Weather App/Scripts/forecast.js
@@ -0,0 +1,37 @@
+const key = "TO2yj0fcAch71sBptC1V3xJOEmGRFElY"; 
+
+//get weather information
+const getWeather = async (id) => {
+  
+    const base = 'http://dataservice.accuweather.com/currentconditions/v1/';
+    const query = `${id}?apikey=${key}`;
+  
+    const response = await fetch(base + query);
+    const data = await response.json();
+  
+    return data[0];
+  
+  };
+
+
+//get city information
+const getCity = async (city) => {
+
+    const base = 'http://dataservice.accuweather.com/locations/v1/cities/search';
+    const query = `?apikey=${key}&q=${city}`;
+  
+    const response = await fetch(base + query);
+    const data = await response.json();
+  
+    return data[0];
+  
+  };
+
+getCity('Nagpur')
+    .then(data => {
+        return getWeather(data.Key);
+    }).then(data => {
+        console.log(data);
+    })
+    .catch(err => console.log(err));
+
